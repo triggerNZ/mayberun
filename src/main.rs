cb8fbc92d438d3b6c84b7de7866c6006d3043849
Tin Pavlinic <tin.pavlinic@gmail.com>
2024-10-05 13:08:32 +1000

    handle out glob

--- src/main.rs
+++ src/main.rs
@@ -15,22 +15,33 @@ fn main() -> io::Result<()> {
     match head {
         None => (),
         Some(cmd) => {
-            let before = if let Some(input) = input_glob {
-                check_glob(&env::current_dir().unwrap(), &input)?
-            } else {
-                CheckResult::Changed
-            };
-
-            let (after, out_glob) = if let Some(output) = output_glob {
-                (check_glob(&cwd, &output)?, Some(output))
+            let (before, in_glob) = if let Some(input) = input_glob {
+                (
+                    check_glob(&env::current_dir().unwrap(), &input)?,
+                    Some(input),
+                )
             } else {
                 (CheckResult::Changed, None)
             };
 
-            if before == CheckResult::Changed || after == CheckResult::Changed {
+            let (after, out_glob) = if let Some(output) = output_glob {
+                (Some(check_glob(&cwd, &output)?), Some(output))
+            } else {
+                (None, None)
+            };
+
+            if before == CheckResult::Changed || after == Some(CheckResult::Changed) {
+                println!("before: {:?}, after: {:?}", before, after);
                 let tail: &[String] = &args[1..];
                 let mut child = Command::new(cmd).args(tail).spawn().expect("Failed");
                 let result = child.wait();
+                if let Some(inp) = in_glob {
+                    match result {
+                        Ok(_) => write_glob(&cwd, &inp)?,
+                        Err(_) => (),
+                    }
+                }
+
                 if let Some(out) = out_glob {
                     match result {
                         Ok(_) => write_glob(&cwd, &out)?,
